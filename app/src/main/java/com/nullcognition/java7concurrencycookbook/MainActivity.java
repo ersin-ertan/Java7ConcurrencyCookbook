cfd75bab1fda6f33a2c8c33098c464e6690867f7
ersin <ersin_the_ertan@hotmail.com>
2015-04-26 23:58:29 -0400

    Simple thread, now Thread info examples

--- app/src/main/java/com/nullcognition/java7concurrencycookbook/MainActivity.java
+++ app/src/main/java/com/nullcognition/java7concurrencycookbook/MainActivity.java
@@ -1,7 +1,8 @@
 package com.nullcognition.java7concurrencycookbook;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,27 +14,70 @@ public class MainActivity extends ActionBarActivity {
 
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_main);
+
+	  // simpleThread();
+	  threadInfo();
+   }
+
+   private void threadInfo(){
+
+	  Thread[] threads = new Thread[10];
+	  Thread.State[] states = new Thread.State[10];
+
+	  for(int i = 0; i < 10; i++){
+		 if((i % 2) == 0){
+			threads[i].setPriority(Thread.MAX_PRIORITY);
+		 }
+		 else{ threads[i].setPriority(Thread.MIN_PRIORITY); }
+		 threads[i].setName("Thread @ " + i);
+	  }
+   }
+
+   private void simpleThread(){
+
+	  for(int i = 0; i < 5; i++){
+		 new Thread(new NewThread(i)).start();
+	  }
+
    }
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu){
-	  // Inflate the menu; this adds items to the action bar if it is present.
+
 	  getMenuInflater().inflate(R.menu.menu_main, menu);
 	  return true;
    }
 
    @Override
    public boolean onOptionsItemSelected(MenuItem item){
-	  // Handle action bar item clicks here. The action bar will
-	  // automatically handle clicks on the Home/Up button, so long
-	  // as you specify a parent activity in AndroidManifest.xml.
+
 	  int id = item.getItemId();
 
-	  //noinspection SimplifiableIfStatement
 	  if(id == R.id.action_settings){
 		 return true;
 	  }
 
 	  return super.onOptionsItemSelected(item);
+   }
+}
+
+class NewThread implements Runnable {
+
+   int i;
+
+   public NewThread(int inI){ i = inI;}
+
+   @Override
+   public void run(){
+
+	  logCurrentThread();
+
+
+   }
+
+   private void logCurrentThread(){
+
+	  Log.e(getClass().getSimpleName(), Thread.currentThread()
+											  .getName() + " " + i);
    }
 }
