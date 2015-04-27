cc6edb0c0fc342be006c5c5178e41be33671ded2
ersin <ersin_the_ertan@hotmail.com>
2015-04-27 00:37:30 -0400

    setting thread proirities, and reading state

--- app/src/main/java/com/nullcognition/java7concurrencycookbook/MainActivity.java
+++ app/src/main/java/com/nullcognition/java7concurrencycookbook/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Logcat stopped working, required restart. Not sure why it happened.
 
 public class MainActivity extends ActionBarActivity {
 
@@ -16,21 +17,57 @@ public class MainActivity extends ActionBarActivity {
 	  setContentView(R.layout.activity_main);
 
 	  // simpleThread();
+	  Log.e(getClass().getSimpleName(), "starting thread info method");
 	  threadInfo();
    }
 
    private void threadInfo(){
 
+	  Log.e(getClass().getSimpleName(), "started thread info method");
+
+
 	  Thread[] threads = new Thread[10];
 	  Thread.State[] states = new Thread.State[10];
 
 	  for(int i = 0; i < 10; i++){
+
+		 threads[i] = new Thread(new NewThread(i));
+
 		 if((i % 2) == 0){
 			threads[i].setPriority(Thread.MAX_PRIORITY);
 		 }
 		 else{ threads[i].setPriority(Thread.MIN_PRIORITY); }
-		 threads[i].setName("Thread @ " + i);
+
+		 states[i] = threads[i].getState();
+		 threads[i].setName("Thread @ " + i + " state is " + states[i]);
+		 Log.e(getClass().getSimpleName(), threads[i].getName());
+
 	  }
+
+	  for(int i = 0; i < 10; i++){
+		 threads[i].start();
+	  }
+
+	  boolean finished = false;
+
+	  do{
+
+		 finished = true;
+
+		 for(int i = 0; i < 10; i++){
+
+			Thread.State state = threads[i].getState();
+
+			if(state != states[i]){
+			   states[i] = state;
+			   Log.e(getClass().getSimpleName(), threads[i].getState().toString());
+			}
+
+			finished = finished && (states[i] == Thread.State.TERMINATED);
+		 }
+
+	  }
+	  while(! finished);
    }
 
    private void simpleThread(){
